6518a2349e5ed936823a2825904f018faaedaa50
leo <diegote142@gmail.com>
2015-01-22 12:01:06 -0500

    Moved EditText to utilities.py

--- utilities.py
+++ utilities.py
@@ -3,6 +3,7 @@
 
 import wx
 import inspect
+import wx.lib.stattext as st
 
 
 ######################
@@ -64,6 +65,108 @@ class AutoSize(wx.ScrolledWindow):
         print "new sz: ", self.content_sz
         self.SetVirtualSize(self.content_sz)
 
+
+
+class Text(st.GenStaticText):
+    def __init__(self, parent, id=wx.ID_ANY, label="", size=wx.DefaultSize, style=0):
+        super(Text, self).__init__(parent, label=label, style=wx.BORDER_NONE, size=size)
+
+    def AcceptsFocus(self):
+        return True
+
+    def AcceptsFocusFromKeyboard(self):
+        return True
+
+
+    
+class EditText(wx.Panel):
+    DEFAULT_SZ = (200, 25)
+    DEFAULT_FONT = (12, wx.SWISS, wx.ITALIC, wx.BOLD)
+    
+    def __init__(self, parent, id = wx.ID_ANY, label="", pos=wx.DefaultPosition, size=DEFAULT_SZ, style=wx.BORDER_NONE|wx.TAB_TRAVERSAL):
+        super(EditText, self).__init__(parent, id=id, pos=pos, size=size, style=style)
+
+        # self.text  = st.GenStaticText(self, label=label, style=wx.BORDER_NONE, size=size)
+        self.text  = Text(self, label=label, style=wx.BORDER_NONE, size=size)
+        self.entry = wx.TextCtrl(self, value=label,
+                                 style=wx.BORDER_SUNKEN|wx.TE_PROCESS_ENTER|wx.TE_MULTILINE,
+                                 size=size)
+
+        box = wx.BoxSizer(wx.HORIZONTAL)
+        box.Add(self.text,  proportion=1, flag=wx.LEFT|wx.EXPAND, border=5)
+        box.Add(self.entry, proportion=1, flag=wx.ALL|wx.EXPAND,  border=0)
+        self.SetSizer(box)
+
+        self.text.Bind(wx.EVT_LEFT_DOWN, self.ShowEntry)
+        self.text.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
+        self.entry.Bind(wx.EVT_LEFT_DOWN, self.ShowText)
+        self.entry.Bind(wx.EVT_TEXT_ENTER, self.ShowText)
+        self.entry.Bind(wx.EVT_KILL_FOCUS, self.ShowText)
+        self.Bind(wx.EVT_SET_FOCUS, self.OnFocus)
+
+        self.text.SetFont(wx.Font(self.DEFAULT_FONT[0], self.DEFAULT_FONT[1], self.DEFAULT_FONT[2], self.DEFAULT_FONT[3]))
+        self.text.Show()
+        self.entry.Hide()
+        self.Show()
+
+
+    ### Behavior functions
+
+    def SetHint(self, txt):
+        # note we are not calling SetLabel on purpose.
+        # we only need to change the label of text, not of entry
+        self.text.SetLabel(txt)
+
+    def SetFont(self, font):
+        self.text.SetFont(font)
+
+    def SetBackgroundColour(self, cl):
+        super(EditText, self).SetBackgroundColour(cl)
+        self.text.SetBackgroundColour(cl)
+
+    def SetEntryColour(self, cl):
+        self.entry.SetBackgroundColour(cl)
+
+    def SetLabel(self, lbl):
+        self.text.SetLabel(lbl)
+        self.entry.SetValue(lbl)
+        return super(EditText, self).SetLabel(lbl)        
+
+    def GetLabel(self):
+        return self.text.GetLabel()
+
+    def ShowEntry(self, ev):
+        print self.text.AcceptsFocus()
+        print self.text.AcceptsFocusFromKeyboard()
+        self.entry.Show()
+        self.entry.SetFocus()
+
+    def ShowText(self, ev):
+        self.entry.Hide()
+        self.text.SetLabel(self.entry.GetValue())
+        self.text.Show()
+        self.text.SetFocus()
+
+
+    ### Auxiliary functions
+
+    
+    ### Callbacks
+
+    def OnKeyDown(self, ev):
+        code = ev.GetKeyCode()
+        if code == 9:
+            ev.Skip()
+        if code == 13 or code == 32 or code in range(65, 91) or code in range(97, 123):
+            self.ShowEntry(None) # ShowEntry doesn't really use its event
+
+    def OnFocus(self, ev):
+        print "receiving focus"
+        if self.text.IsShown():
+            self.text.SetFocus()
+        else:
+            self.entry.SetFocus()
+        
         
 
 ######################
