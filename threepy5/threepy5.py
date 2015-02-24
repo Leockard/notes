928bf1cbf8f4af73a34f46adc236e951e1567cec
leo <diegote142@gmail.com>
2015-02-24 15:33:18 -0500

    Rewrite threepy5 to use classfactory.py

--- threepy5/threepy5.py
+++ threepy5/threepy5.py
@@ -11,7 +11,7 @@ import classfactory as fac
 # Card classes
 ######################
 
-CardBase = fac.class_prop_creator("CardBase", base=object, id=-1, rect=(0,0,-1,-1))
+CardBase = fac.class_prop_creator("CardBase", id=-1, rect=(0,0,-1,-1))
 class Card(CardBase):
     """`Card` is a "virtual 3x5 index card". They are assumed to lie on a
     surface and relative position to one another is very important.
@@ -30,6 +30,7 @@ class Card(CardBase):
         """
         super(Card, self).__init__(**kwargs)
 Card = fac.pub_class(Card, pub)
+
 
 
 
@@ -88,146 +89,30 @@ class Content(ContentBase):
         super(Content, self).__init__(**kwargs)
 Content = fac.pub_class(Content, pub)
 
-# class Content(Card):
-#     """
-#     A `Card` which holds text contents. Features: title, kind, rating, content.
-    
-#     In its content text field, the user may input "tags". Any line of the form
-#     ^my-tag: foo bar baz$
-#     is considered to define the tag "my-tag". Tag names (before the colon) must
-#     be single words, and their content (after the colon) may be any string,
-#     until a newline.
-    
-#     A tag can be anything, though they usually describe facts about concepts:
-
-#         Content Card "Protein"
-#         kind: concept
-#         rating: 2 stars
-#             Proteins are chains of amino-acids which...
-#             Number: there are x types of proteins.
-#             Kinds: transmembrane proteins, integral membrane proteins.
-
-#     This `Content` has two tags: "number" and "kinds".
-
-#     A `Content` can be "collapsed". This means that its content text is hidden
-#     and we only wish to display its title.
-#     """
-#     KIND_DEFAULT    = "kind"
-#     KIND_CONCEPT    = "C"
-#     KIND_RESEARCH   = "R"
-#     KIND_ASSUMPTION = "A"
-#     KIND_FACT       = "F"
-
-#     RATING_MAX = 3
-
-#     UPDATE_TITLE     = "title updated"
-#     UPDATE_KIND      = "kind updated"
-#     UPDATE_CONTENT   = "content updated"
-#     UPDATE_RATING    = "rating updated"
-#     UPDATE_COLLAPSED =  "collapsed updated"
-    
-    
-#     def __init__(self, id=Card.DEFAULT_ID, rect=Card.DEFAULT_RECT, title="",
-#                  kind=KIND_DEFAULT, content="", rating=0, collapsed=False):
-#         """Constructor.
-
-#         * `id: ` identification number. 
-#         * `rect: ` (x, y, w, h), accepts floats.
-#         * `kind: ` one of `Content.KIND_*`.
-#         * `content: ` the content text.
-#         * `rating: ` a measure of the importance of this `Content`. Must be an
-#         int from 0 to `RATING_MAX`, inclusive.
-#         * `collapsed: ` if `True`, we ignore the contents. In that case, this
-#         `Content` would funtion sort of like a `Header` with a kind and a rating.
-#         """
-#         super(Content, self).__init__(id, rect)
-#         self._title = title
-#         self._kind = kind
-#         self._content = content
-#         self._rating = rating
-#         self._collapsed = collapsed
-
-#     @property
-#     def title(self):
-#         return self._title
-
-#     @title.setter
-#     def title(self, val):
-#         pub.sendMessage(self.UPDATE_TITLE)
-#         self._title = val
-
-#     @property
-#     def kind(self):
-#         return self._kind
-
-#     @kind.setter
-#     def kind(self, val):
-#         pub.sendMessage(self.UPDATE_KIND)
-#         self._kind = val
-
-#     @property
-#     def content(self):
-#         return self._content
-
-#     @content.setter
-#     def content(self, val):
-#         pub.sendMessage(self.UPDATE_CONTENT)
-#         self._content = val
-
-#     @property
-#     def rating(self):
-#         return self._rating
-
-#     @rating.setter
-#     def rating(self, val):
-#         pub.sendMessage(self.UPDATE_RATING)
-#         self._rating = val
-
-#     @property
-#     def collapsed(self):
-#         return self._collapsed
-
-#     @collapsed.setter
-#     def collapsed(self, val):
-#         pub.sendMessage(self.UPDATE_COLLAPSED)
-#         self._collapsed = val
-
         
-                
-class Header(Card):
+HeaderBase = fac.class_prop_creator("HeaderBase", base=Card, header="")                
+class Header(HeaderBase):
     """`Card` that holds a title or header."""
 
-    UPDATE_HEADER = "header updated"
-
-    def __init__(self, id=Card.DEFAULT_ID, rect=Card.DEFAULT_RECT, header=""):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `id: ` identification number. 
         * `rect: ` (x, y, w, h), accepts floats.
         * `header: ` the title or header.
         """
-        super(Header, self).__init__(id, rect)
-        self._header = header
+        super(Header, self).__init__(**kwargs)
+Header = fac.pub_class(Header, pub)
 
-    @property
-    def header(self):
-        return self._collapsed
 
-    @header.setter
-    def header(self, val):
-        pub.sendMessage(self.UPDATE_HEADER)
-        self._header = val
-
-        
-class Image(Card):
+ImageBase = fac.class_prop_creator("ImageBase", base=Card, path="", scale=1.0)                        
+class Image(ImageBase):
     """A `Card` that holds a single image. Note that this class doesn't
     actually load the image from disk. If the application needs to display
     the image, it must load it by itself.
     """
 
-    UPDATE_PATH = "updated path"
-
-    def __init__(self, id=Card.DEFAULT_ID, rect=Card.DEFAULT_RECT, path="", scale=1.0):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `id: ` identification number. 
@@ -236,27 +121,8 @@ class Image(Card):
         * `scale: ` the scale at which we show the image. This is the float by which we need
         to resize the original image so that it fits in `self.rect`.
         """
-        super(Image, self).__init__(id, rect)
-        self._path = path
-        self._scale = scale
-
-    @property
-    def path(self):
-        return self._path
-
-    @path.setter
-    def path(self, val):
-        pub.sendMessage(self.UPDATE_PATH)
-        self._path = val
-
-    @property
-    def scale(self):
-        return self._scale
-
-    @scale.setter
-    def scale(self, val):
-        pub.sendMessage(self.UPDATE_SCALE)
-        self._scale = val
+        super(Image, self).__init__(**kwargs)
+Image = fac.pub_class(Image, pub)
 
 
 
@@ -264,85 +130,37 @@ class Image(Card):
 # Annotation class
 ######################
 
-class Line(object):
+LineBase = fac.class_prop_creator("LineBase", base=object, colour=(0,0,0,0), thickness=1, pts=[])                        
+class Line(LineBase):
     """A `Line` represents a single stroke of the annotations or doodles the user draws in the
     infinite surface that the `Card`s are drawn on. These are drawn on top of the `Card`s.
     """
     DEFAULT_COLOUR = (0,0,0,0)
     DEFAULT_THICKNESS = 1
 
-    UPDATE_COLOUR = "updated colour"
-    UPDATE_THICKNESS = "updated thickness"
-    UPDATE_POINTS = "updated points"
-
-    def __init__(self, colour=DEFAULT_COLOUR, thickness=DEFAULT_THICKNESS, pts=[]):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `colour: ` a (r,g,b,alpha) tuple.
         * `thickness: ` an int representing the thickness of this stroke.
         * `pts: ` the points defining this polyline.
         """
-        self._colour = colour
-        self._thickness = thickness
-        self._pts = pts
-
-    @property
-    def colour(self):
-        return self._colour
-
-    @colour.setter
-    def colour(self, val):
-        self._colour = val
-        pub.sendMessage(self.UPDATE_COLOUR)
-
-    @property
-    def thickness(self):
-        return self._thickness
-
-    @thickness.setter
-    def thickness(self, val):
-        self._thickness = val
-        pub.sendMessage(self.UPDATE_THICKNESS)
-
-    @property
-    def pts(self):
-        return self._pts
-
-    @pts.setter
-    def pts(self, val):
-        self._pts = val
-        pub.sendMessage(self.UPDATE_POINTS)
-
-    def AddPoint(self, pt):
-        self._pts.append(pt)
-        pub.sendMessage(self.UPDATE_POINTS)
+        super(Line, self).__init__(**kwargs)
+Line = fac.pub_class(Line, pub)
 
     
 
-class Annotation(object):
+AnnotationBase = fac.class_prop_creator("AnnotationBase", lines=[])                        
+class Annotation(AnnotationBase):
     """`Annotation` is the set of all `Line`s over an `AnnotatedDeck` of `Card`s."""
 
-    UPDATE_LINES = "updated lines"
-
-    def __init__(self, lines = []):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `lines: ` a list of `Line`s.
         """
-        self._lines = lines
-
-    @property
-    def lines(self):
-        return self._lines
-
-    @lines.setter
-    def lines(self, val):
-        self._lines = val
-        pub.sendMessage(self.UPDATE_LINES)
-
-    def AddLine(self, line):
-        self._lines.append(line)
-        pub.sendMessage(self.UPDATE_LINES)
+        super(Annotation, self).__init__(**kwargs)
+Annotation = fac.pub_class(Annotation, pub)
 
 
 
@@ -350,65 +168,49 @@ class Annotation(object):
 # Collections of Cards
 ##########################        
 
+
+CardGroupBase = fac.class_prop_creator("CardGroupBase", id=-1, members=[])                        
 class CardGroup(object):
     """A list of `Card`s. Grouped `Card`s have meaning together. A `Card` may
     belong to more than one group. If all the `Card`s in one group are also in
     another group, the smaller group is considered nested in the larger one.
     """
 
-    UPDATE_MEMBERS = "updated members"
-
-    def __init__(self, id=-1, members=[]):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `id: ` idenfitication number.
         * `members: ` a list of identification numbers from `Card`s.
         """
-        self._id = id
-        self._members = []
-
-    @property
-    def members(self):
-        return self._members
-
-    @members.setter
-    def members(self, val):
-        self._members = val
-        pub.sendMessage(self.UPDATE_MEMBERS)
-
-    def AddCard(self, card):
-        self._members.append(card)
-        pub.sendMessage(self.UPDATE_MEMBERS)
-
-    def RemoveCard(self, card):
-        if card in self._members:
-            self._members.remove(card)
-        pub.sendMessage(self.UPDATE_MEMBERS)
+        super(CardGroup, self).__init__(**kwargs)
+CardGroup = fac.pub_class(CardGroup, pub)
 
 
 
-class Deck(object):
+DeckBase = fac.class_prop_creator("DeckBase", name="", id=-1, members=[])                        
+class Deck(DeckBase):
     """It's a collection of `Card`s that share a common topic. It can also hold
     many `CardGroup`s. A `Card` from a `Deck` may have the same id as a `CardGroup`
     but not the same id as another `Card` from the same `Deck`.
     """
 
-    def __init__(self, name="", cards=[], groups=[]):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `name: ` the name of this `Deck`.
         * `cards: ` a list of `Card`s.
         * `groups: ` a list of `CardGroup`s.
         """
-        self.cards = cards
-        self.groups = groups
+        super(Deck, self).__init__(**kwargs)
+Deck = fac.pub_class(Deck, pub)
 
 
 
-class AnnotatedDeck(Deck):
+AnnotatedDeckBase = fac.class_prop_creator("AnnotatedDeckBase", base=Deck, name="", cards=[], groups=[], lines=[])                        
+class AnnotatedDeck(AnnotatedDeckBase):
     """A collection of `Card`s that can be annotated on."""
 
-    def __init__(self, name="", cards=[], groups=[], lines=[]):
+    def __init__(self, **kwargs):
         """Constructor.
 
         * `name: ` the name of this `Deck`.
@@ -416,10 +218,13 @@ class AnnotatedDeck(Deck):
         * `groups: ` a list of `CardGroup`s.
         * `lines: ` a list of `Line`s.
         """
-        super(AnnotatedDeck, self).__init__(cards, groups)
+        super(AnnotatedDeck, self).__init__(**kwargs)
         self.annotation = Annotation(lines)
+AnnotatedDeck = fac.pub_class(AnnotatedDeck, pub)
     
 
+
+BoxBase = fac.class_prop_creator("BoxBase", name="", path="", decks=[])                                
 class Box(object):
     """A `Box` holds various `Deck`s. It is the equivalent of a file at
     application level: every `Box` is stored in one file and every file
@@ -433,6 +238,6 @@ class Box(object):
         * `path: ` the path to the file on disk.
         * `decks: ` a list of `Deck`s (or `AnnotatedDeck`s).
         """
-        self.name = name
-        self.path = path
-        self.decks = decks
+        super(Box, self).__init__(**kwargs)
+Box = fac.pub_class(Box, pub)
+
