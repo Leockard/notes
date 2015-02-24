4ead08107ee19f880169e523e0084badf971de47
leo <diegote142@gmail.com>
2015-02-24 15:34:28 -0500

    Code rearrangement.

--- threepy5/threepy5.py
+++ threepy5/threepy5.py
@@ -24,7 +24,7 @@ class Card(CardBase):
 
     def __init__(self, **kwargs):
         """Constructor.
-        
+
         * `id: ` this `Card`'s identification number.
         * `rect: ` (x, y, w, h), accepts floats.
         """
@@ -38,13 +38,13 @@ ContentBase = fac.class_prop_creator("ContentBase", base=Card, title="", kind="k
 class Content(ContentBase):
     """
     A `Card` which holds text contents. Features: title, kind, rating, content.
-    
+
     In its content text field, the user may input "tags". Any line of the form
         ^my-tag: foo bar baz$
     is considered to define the tag "my-tag". Tag names (before the colon) must
     be single words, and their content (after the colon) may be any string,
     until a newline.
-    
+
     A tag can be anything, though they usually describe facts about concepts:
 
         Content Card "Protein"
@@ -73,11 +73,11 @@ class Content(ContentBase):
     UPDATE_RATING    = "rating updated"
     UPDATE_COLLAPSED =  "collapsed updated"
 
-        
+
     def __init__(self, **kwargs):
         """Constructor.
 
-        * `id: ` identification number. 
+        * `id: ` identification number.
         * `rect: ` (x, y, w, h), accepts floats.
         * `kind: ` one of `Content.KIND_*`.
         * `content: ` the content text.
@@ -89,15 +89,15 @@ class Content(ContentBase):
         super(Content, self).__init__(**kwargs)
 Content = fac.pub_class(Content, pub)
 
-        
-HeaderBase = fac.class_prop_creator("HeaderBase", base=Card, header="")                
+
+HeaderBase = fac.class_prop_creator("HeaderBase", base=Card, header="")
 class Header(HeaderBase):
     """`Card` that holds a title or header."""
 
     def __init__(self, **kwargs):
         """Constructor.
 
-        * `id: ` identification number. 
+        * `id: ` identification number.
         * `rect: ` (x, y, w, h), accepts floats.
         * `header: ` the title or header.
         """
@@ -105,7 +105,7 @@ class Header(HeaderBase):
 Header = fac.pub_class(Header, pub)
 
 
-ImageBase = fac.class_prop_creator("ImageBase", base=Card, path="", scale=1.0)                        
+ImageBase = fac.class_prop_creator("ImageBase", base=Card, path="", scale=1.0)
 class Image(ImageBase):
     """A `Card` that holds a single image. Note that this class doesn't
     actually load the image from disk. If the application needs to display
@@ -115,7 +115,7 @@ class Image(ImageBase):
     def __init__(self, **kwargs):
         """Constructor.
 
-        * `id: ` identification number. 
+        * `id: ` identification number.
         * `rect: ` (x, y, w, h), accepts floats.
         * `path: ` the path to the image on disk.
         * `scale: ` the scale at which we show the image. This is the float by which we need
@@ -130,7 +130,7 @@ Image = fac.pub_class(Image, pub)
 # Annotation class
 ######################
 
-LineBase = fac.class_prop_creator("LineBase", base=object, colour=(0,0,0,0), thickness=1, pts=[])                        
+LineBase = fac.class_prop_creator("LineBase", base=object, colour=(0,0,0,0), thickness=1, pts=[])
 class Line(LineBase):
     """A `Line` represents a single stroke of the annotations or doodles the user draws in the
     infinite surface that the `Card`s are drawn on. These are drawn on top of the `Card`s.
@@ -148,9 +148,9 @@ class Line(LineBase):
         super(Line, self).__init__(**kwargs)
 Line = fac.pub_class(Line, pub)
 
-    
 
-AnnotationBase = fac.class_prop_creator("AnnotationBase", lines=[])                        
+
+AnnotationBase = fac.class_prop_creator("AnnotationBase", lines=[])
 class Annotation(AnnotationBase):
     """`Annotation` is the set of all `Line`s over an `AnnotatedDeck` of `Card`s."""
 
@@ -166,10 +166,9 @@ Annotation = fac.pub_class(Annotation, pub)
 
 ##########################
 # Collections of Cards
-##########################        
+##########################
 
-
-CardGroupBase = fac.class_prop_creator("CardGroupBase", id=-1, members=[])                        
+CardGroupBase = fac.class_prop_creator("CardGroupBase", id=-1, members=[])
 class CardGroup(object):
     """A list of `Card`s. Grouped `Card`s have meaning together. A `Card` may
     belong to more than one group. If all the `Card`s in one group are also in
@@ -187,7 +186,7 @@ CardGroup = fac.pub_class(CardGroup, pub)
 
 
 
-DeckBase = fac.class_prop_creator("DeckBase", name="", id=-1, members=[])                        
+DeckBase = fac.class_prop_creator("DeckBase", name="", id=-1, members=[])
 class Deck(DeckBase):
     """It's a collection of `Card`s that share a common topic. It can also hold
     many `CardGroup`s. A `Card` from a `Deck` may have the same id as a `CardGroup`
@@ -206,7 +205,11 @@ Deck = fac.pub_class(Deck, pub)
 
 
 
-AnnotatedDeckBase = fac.class_prop_creator("AnnotatedDeckBase", base=Deck, name="", cards=[], groups=[], lines=[])                        
+##################################
+# Collections of mixed objects
+##################################
+
+AnnotatedDeckBase = fac.class_prop_creator("AnnotatedDeckBase", base=Deck, name="", cards=[], groups=[], lines=[])
 class AnnotatedDeck(AnnotatedDeckBase):
     """A collection of `Card`s that can be annotated on."""
 
@@ -221,10 +224,10 @@ class AnnotatedDeck(AnnotatedDeckBase):
         super(AnnotatedDeck, self).__init__(**kwargs)
         self.annotation = Annotation(lines)
 AnnotatedDeck = fac.pub_class(AnnotatedDeck, pub)
-    
 
 
-BoxBase = fac.class_prop_creator("BoxBase", name="", path="", decks=[])                                
+
+BoxBase = fac.class_prop_creator("BoxBase", name="", path="", decks=[])
 class Box(object):
     """A `Box` holds various `Deck`s. It is the equivalent of a file at
     application level: every `Box` is stored in one file and every file
@@ -240,4 +243,3 @@ class Box(object):
         """
         super(Box, self).__init__(**kwargs)
 Box = fac.pub_class(Box, pub)
-
