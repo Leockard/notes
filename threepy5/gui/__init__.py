d2339e3f75ec4ab68fc21889795f73f614ce702e
leo <diegote142@gmail.com>
2015-02-28 16:49:06 -0500

    refactor(files) Move the old gui files out of the way.

--- threepy5/gui/__init__.py
+++ threepy5/gui/__init__.py
@@ -1,10 +1,4 @@
 """
 Modules in gui hold the view/controller classes for `threepy5`.
 """
-import card
-import deck
-import box
-import canvas
-import view
-import wxutils
-import threepy5GUI
+import threepy5GUI_new
