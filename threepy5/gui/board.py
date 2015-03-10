6d4740e2cdd6292e80f6f4a2fd12f95d30def448
leo <diegote142@gmail.com>
2015-03-09 21:22:00 -0500

    feat: added the handler for capture lost events.

--- threepy5/gui/board.py
+++ threepy5/gui/board.py
@@ -517,14 +517,20 @@ class ImageWin(CardWin):
         self.SetCursor(wx.NullCursor)
 
     def _on_border_left_down(self, ev):
-        self.CaptureMouse()                
+        self.CaptureMouse()
+        self.Bind(wx.EVT_MOUSE_CAPTURE_LOST, self._on_capture_lost)
         self._resize_init()
 
+    def _on_capture_lost(self, ev):
+        self.ReleaseMouse()
+        self.Unbind(wx.EVT_MOUSE_CAPTURE_LOST, self._on_capture_lost)
+        
     def _on_border_left_up(self, ev):
         # since we captured the mouse, pos is in coordinates relative to
         # this window, while we need it relative to the board
         self._resize_end(self.Position + ev.Position)
-        self.ReleaseMouse()        
+        self.ReleaseMouse()
+        self.Unbind(wx.EVT_MOUSE_CAPTURE_LOST, self._on_capture_lost)
 
     def _on_border_motion(self, ev):
         # since we captured the mouse, pos is in coordinates relative to
