91f700ac47006c7f053181976547b70055854f49
leo <diegote142@gmail.com>
2015-03-13 10:59:44 -0500

    refactor.

--- threepy5/gui/old/view.py
+++ threepy5/gui/old/view.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-`*View` classes are used to take a closer look at certain objects.
-"""
+# """
+# `*View` classes are used to take a closer look at certain objects.
+# """
 
-import wx
-import re
-import card
-from deck import Deck
-import wxutils
+# import wx
+# import re
+# import card
+# from deck import Deck
+# import wxutils
 
 
 ######################
@@ -136,101 +136,101 @@ class DeckView(wxutils.AutoSize):
             
 
 
-######################
-# CardView Class
-######################        
+# ######################
+# # CardView Class
+# ######################        
 
-class CardView(wx.Panel):
-    """Displays a screen-sized `Content` `Card` to facilitate editing. While
-    viewing, the `Card`s are `Reparent`ed to this window.
-    """
+# class CardView(wx.Panel):
+#     """Displays a screen-sized `Content` `Card` to facilitate editing. While
+#     viewing, the `Card`s are `Reparent`ed to this window.
+#     """
     
-    CARD_PADDING = Deck.CARD_PADDING
-    BACKGROUND_CL = "#CCCCCC"
+#     CARD_PADDING = Deck.CARD_PADDING
+#     BACKGROUND_CL = "#CCCCCC"
     
-    TITLE_FONT   = (18, wx.SWISS, wx.ITALIC, wx.BOLD)
-    CONTENT_FONT = (14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+#     TITLE_FONT   = (18, wx.SWISS, wx.ITALIC, wx.BOLD)
+#     CONTENT_FONT = (14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
 
-    def __init__(self, parent, cards=[], pos=wx.DefaultPosition, size=wx.DefaultSize):
-        """Constructor.
+#     def __init__(self, parent, cards=[], pos=wx.DefaultPosition, size=wx.DefaultSize):
+#         """Constructor.
 
-        * `parent: ` the parent `Box`.
-        * `cards: ` the `Card`s we are viewing.
-        * `pos: ` by default, is `wx.DefaultSize`.
-        * `size: ` by default, is `wx.DefaultSize`.
-        """
-        super(CardView, self).__init__(parent, size=size)
+#         * `parent: ` the parent `Box`.
+#         * `cards: ` the `Card`s we are viewing.
+#         * `pos: ` by default, is `wx.DefaultSize`.
+#         * `size: ` by default, is `wx.DefaultSize`.
+#         """
+#         super(CardView, self).__init__(parent, size=size)
         
-        # GUI
-        self.SetBackgroundColour(self.BACKGROUND_CL)
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
-        self.SetSizer(hbox)
+#         # GUI
+#         self.SetBackgroundColour(self.BACKGROUND_CL)
+#         hbox = wx.BoxSizer(wx.HORIZONTAL)
+#         self.SetSizer(hbox)
 
-        # members
-        self.cards = {}
+#         # members
+#         self.cards = {}
 
                 
-    ### Behavior functions
+#     ### Behavior functions
 
-    def GetCards(self):
-        """Returns the `Card`s currently under viewing.
+#     def GetCards(self):
+#         """Returns the `Card`s currently under viewing.
 
-        `returns: ` a `list` of `Card`s.
-        """
-        return self.cards.keys()
+#         `returns: ` a `list` of `Card`s.
+#         """
+#         return self.cards.keys()
 
-    def AddCard(self, crd):
-        """Adds one `Card` to the viewing control.
+#     def AddCard(self, crd):
+#         """Adds one `Card` to the viewing control.
 
-        * `card: ` a `Card`.
-        """
-        # setup and reparent: wil restore parent when done. See RestoreCards.
-        self.cards[crd] = {}
-        self.cards[crd]["parent"] = crd.GetParent()
-        self.cards[crd]["rect"] = crd.GetRect()
-        crd.Reparent(self)
-        crd.SetViewing(True)
-        crd.content.SetFocus()
+#         * `card: ` a `Card`.
+#         """
+#         # setup and reparent: wil restore parent when done. See RestoreCards.
+#         self.cards[crd] = {}
+#         self.cards[crd]["parent"] = crd.GetParent()
+#         self.cards[crd]["rect"] = crd.GetRect()
+#         crd.Reparent(self)
+#         crd.SetViewing(True)
+#         crd.content.SetFocus()
         
-        # setup UI
-        box = self.GetSizer()
-        box.Add(crd, proportion=1, flag=wx.ALL|wx.EXPAND, border=self.CARD_PADDING)
-        box.Layout()
+#         # setup UI
+#         box = self.GetSizer()
+#         box.Add(crd, proportion=1, flag=wx.ALL|wx.EXPAND, border=self.CARD_PADDING)
+#         box.Layout()
 
-        # bindings
-        crd.Bind(card.Card.EVT_CANCEL_VIEW, self.OnCancelView)
+#         # bindings
+#         crd.Bind(card.Card.EVT_CANCEL_VIEW, self.OnCancelView)
 
-    def SetCards(self, cards):
-        """Clears previous `Card`s and views the new ones.
+#     def SetCards(self, cards):
+#         """Clears previous `Card`s and views the new ones.
 
-        * `cards: ` a `list` of `Card`s.
-        """
-        self.Clear()
-        for c in cards: self.AddCard(c)
+#         * `cards: ` a `list` of `Card`s.
+#         """
+#         self.Clear()
+#         for c in cards: self.AddCard(c)
 
-    def Restore(self):
-        """Restores the viewed `Card`s to their original parents and positions."""
-        for c in self.cards:
-            c.Reparent(self.cards[c]["parent"])
-            c.SetRect(self.cards[c]["rect"])
-        self.Clear()
+#     def Restore(self):
+#         """Restores the viewed `Card`s to their original parents and positions."""
+#         for c in self.cards:
+#             c.Reparent(self.cards[c]["parent"])
+#             c.SetRect(self.cards[c]["rect"])
+#         self.Clear()
 
-    def Clear(self):
-        """Clear all viewed `Card`s."""
-        self.GetSizer().Clear()
-        for c in self.cards.keys():
-            c.SetViewing(False)
-        self.cards = {}
+#     def Clear(self):
+#         """Clear all viewed `Card`s."""
+#         self.GetSizer().Clear()
+#         for c in self.cards.keys():
+#             c.SetViewing(False)
+#         self.cards = {}
 
 
-    ### Callbacks
+#     ### Callbacks
 
-    def OnCancelView(self, ev):
-        """Listens to `Card.EVT_CANCEL_VIEW` on every viewed `Card`."""
-        self.Restore()
-        event = card.Card.CancelViewEvent(id=wx.ID_ANY)
-        event.SetEventObject(ev.GetEventObject())
-        self.GetEventHandler().ProcessEvent(event)
+#     def OnCancelView(self, ev):
+#         """Listens to `Card.EVT_CANCEL_VIEW` on every viewed `Card`."""
+#         self.Restore()
+#         event = card.Card.CancelViewEvent(id=wx.ID_ANY)
+#         event.SetEventObject(ev.GetEventObject())
+#         self.GetEventHandler().ProcessEvent(event)
     
 
 
@@ -254,125 +254,125 @@ class MiniCard(wx.Window):
 
 
 
-######################
-# TagView Class
-######################        
+# ######################
+# # TagView Class
+# ######################        
 
-class TagView(wx.Panel):
-    """The sidebard that displays a `Content` `Card`'s tags."""
+# class TagView(wx.Panel):
+#     """The sidebard that displays a `Content` `Card`'s tags."""
 
-    TAGS_REGEX = "^(\w+):(.*)$"
+#     TAGS_REGEX = "^(\w+):(.*)$"
     
-    def __init__(self, parent, deck, pos=wx.DefaultPosition, size=wx.DefaultSize):
-        """Constructor.
+#     def __init__(self, parent, deck, pos=wx.DefaultPosition, size=wx.DefaultSize):
+#         """Constructor.
 
-        * `parent: ` the parent `Box`.
-        * `deck: ` the parent `Deck` of the `Card`s we are viewing.
-        * `pos: ` by default, is `wx.DefaultSize`.
-        * `size: ` by default, is `wx.DefaultSize`.
-        """
-        super(TagView, self).__init__(parent, pos=pos, size=size)
-        self.deck = deck
-        self.InitUI()
+#         * `parent: ` the parent `Box`.
+#         * `deck: ` the parent `Deck` of the `Card`s we are viewing.
+#         * `pos: ` by default, is `wx.DefaultSize`.
+#         * `size: ` by default, is `wx.DefaultSize`.
+#         """
+#         super(TagView, self).__init__(parent, pos=pos, size=size)
+#         self.deck = deck
+#         self.InitUI()
 
-        # bindings
-        self.Bind(wx.EVT_SHOW, self.OnShow)
-        deck.Bind(Deck.EVT_NEW_CARD, self.OnNewCard)
+#         # bindings
+#         self.Bind(wx.EVT_SHOW, self.OnShow)
+#         deck.Bind(Deck.EVT_NEW_CARD, self.OnNewCard)
 
 
-    ### Behavior functions
+#     ### Behavior functions
 
-    def ParseTags(self, txt):
-        """Parses a string looking for tags.
+#     def ParseTags(self, txt):
+#         """Parses a string looking for tags.
 
-        * `txt: ` a string, the contents of a `Content`.
+#         * `txt: ` a string, the contents of a `Content`.
 
-        `returns: ` a string to display in the `TagView` view, representing the tags found in `text`.
-        """
-        string = ""
-        results = re.findall(self.TAGS_REGEX, txt, re.MULTILINE)
-        for tag, val in results:
-            string += tag + ":" + val
-            string += "\n\n"
-        return string
+#         `returns: ` a string to display in the `TagView` view, representing the tags found in `text`.
+#         """
+#         string = ""
+#         results = re.findall(self.TAGS_REGEX, txt, re.MULTILINE)
+#         for tag, val in results:
+#             string += tag + ":" + val
+#             string += "\n\n"
+#         return string
 
-    def ShowTags(self, card):
-        """Shows the `card`'s tags.
+#     def ShowTags(self, card):
+#         """Shows the `card`'s tags.
 
-        * `card: ` a `Content`, whose contents will be parsed.
-        """
-        self.txt.SetValue(self.ParseTags(card.GetContent()))
+#         * `card: ` a `Content`, whose contents will be parsed.
+#         """
+#         self.txt.SetValue(self.ParseTags(card.GetContent()))
     
         
-    ### Auxiliary functions
+#     ### Auxiliary functions
 
-    def InitUI(self):
-        """Initialize this window's GUI and controls."""
-        box = wx.BoxSizer(wx.VERTICAL)
-        self.SetSizer(box)
+#     def InitUI(self):
+#         """Initialize this window's GUI and controls."""
+#         box = wx.BoxSizer(wx.VERTICAL)
+#         self.SetSizer(box)
 
-        txt = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.txt = txt        
-        box.Add(txt, proportion=1, flag=wx.ALL|wx.EXPAND, border=1)
-
-
-    ### Callbacks
-
-    def OnShow(self, ev):
-        """Listens to `wx.EVT_SHOW`."""
-        if ev.IsShown():
-            crd = wxutils.GetCardAncestor(self.FindFocus())
-            if crd and isinstance(crd, card.Content):
-                self.ShowTags(crd)
-
-    def OnNewCard(self, ev):
-        """Listens to `Deck.EVT_NEW_CARD`."""
-        card = ev.GetEventObject()
-        for ch in card.GetChildren():
-            ch.Bind(wx.EVT_SET_FOCUS, self.OnCardChildFocus)
-
-    def OnCardChildFocus(self, ev):
-        """Listens to `wx.EVT_SET_FOCUS` on every `Card`."""
-        card = wxutils.GetCardAncestor(ev.GetEventObject())
-        if self.IsShown():
-            self.ShowTags(card)
-        ev.Skip()
+#         txt = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+#         self.txt = txt        
+#         box.Add(txt, proportion=1, flag=wx.ALL|wx.EXPAND, border=1)
 
 
+#     ### Callbacks
 
-###########################
-# pdoc documentation setup
-###########################
-# __pdoc__ is the special variable from the automatic
-# documentation generator pdoc.
-# By setting pdoc[class.method] to None, we are telling
-# pdoc to not generate documentation for said method.
-__pdoc__ = {}
-__pdoc__["field"] = None
+#     def OnShow(self, ev):
+#         """Listens to `wx.EVT_SHOW`."""
+#         if ev.IsShown():
+#             crd = wxutils.GetCardAncestor(self.FindFocus())
+#             if crd and isinstance(crd, card.Content):
+#                 self.ShowTags(crd)
 
-# Since we only want to generate documentation for our own
-# mehods, and not the ones coming from the base classes,
-# we first set to None every method in the base class.
-for field in dir(wxutils.AutoSize):
-    __pdoc__['DeckView.%s' % field] = None
-for field in dir(wx.Panel):
-    __pdoc__['CardView.%s' % field] = None
-for field in dir(wx.Window):
-    __pdoc__['MiniCard.%s' % field] = None
-for field in dir(wx.Panel):
-    __pdoc__['TagView.%s' % field] = None
+#     def OnNewCard(self, ev):
+#         """Listens to `Deck.EVT_NEW_CARD`."""
+#         card = ev.GetEventObject()
+#         for ch in card.GetChildren():
+#             ch.Bind(wx.EVT_SET_FOCUS, self.OnCardChildFocus)
 
-# Then, we have to add again the methods that we have
-# overriden. See https://github.com/BurntSushi/pdoc/issues/15.
-for field in DeckView.__dict__.keys():
-    if 'DeckView.%s' % field in __pdoc__.keys():
-        del __pdoc__['DeckView.%s' % field]
-for field in CardView.__dict__.keys():
-    if 'CardView.%s' % field in __pdoc__.keys():
-        del __pdoc__['CardView.%s' % field]
-for field in MiniCard.__dict__.keys():
-    if 'MiniCard.%s' % field in __pdoc__.keys():
-        del __pdoc__['MiniCard.%s' % field]
-for field in TagView.__dict__.keys():
-    if 'TagView.%s' % field in __pdoc__.keys():
-        del __pdoc__['TagView.%s' % field]
+#     def OnCardChildFocus(self, ev):
+#         """Listens to `wx.EVT_SET_FOCUS` on every `Card`."""
+#         card = wxutils.GetCardAncestor(ev.GetEventObject())
+#         if self.IsShown():
+#             self.ShowTags(card)
+#         ev.Skip()
+
+
+
+# ###########################
+# # pdoc documentation setup
+# ###########################
+# # __pdoc__ is the special variable from the automatic
+# # documentation generator pdoc.
+# # By setting pdoc[class.method] to None, we are telling
+# # pdoc to not generate documentation for said method.
+# __pdoc__ = {}
+# __pdoc__["field"] = None
+
+# # Since we only want to generate documentation for our own
+# # mehods, and not the ones coming from the base classes,
+# # we first set to None every method in the base class.
+# for field in dir(wxutils.AutoSize):
+#     __pdoc__['DeckView.%s' % field] = None
+# for field in dir(wx.Panel):
+#     __pdoc__['CardView.%s' % field] = None
+# for field in dir(wx.Window):
+#     __pdoc__['MiniCard.%s' % field] = None
+# for field in dir(wx.Panel):
+#     __pdoc__['TagView.%s' % field] = None
+
+# # Then, we have to add again the methods that we have
+# # overriden. See https://github.com/BurntSushi/pdoc/issues/15.
+# for field in DeckView.__dict__.keys():
+#     if 'DeckView.%s' % field in __pdoc__.keys():
+#         del __pdoc__['DeckView.%s' % field]
+# for field in CardView.__dict__.keys():
+#     if 'CardView.%s' % field in __pdoc__.keys():
+#         del __pdoc__['CardView.%s' % field]
+# for field in MiniCard.__dict__.keys():
+#     if 'MiniCard.%s' % field in __pdoc__.keys():
+#         del __pdoc__['MiniCard.%s' % field]
+# for field in TagView.__dict__.keys():
+#     if 'TagView.%s' % field in __pdoc__.keys():
+#         del __pdoc__['TagView.%s' % field]
