8ce6d998cf4ee3a1d481033de3d92e69ffa6bf89
leo <diegote142@gmail.com>
2015-03-14 09:35:12 -0500

    refactor: some code moved to gui/frame.py.

--- threepy5/test/frame_test.py
+++ threepy5/test/frame_test.py
@@ -13,52 +13,13 @@ class TestFrame(wx.Frame):
         super(TestFrame, self).__init__(parent)
         box = wx.BoxSizer(wx.VERTICAL)
         self.Sizer = box
-
-        dbg = wx.Button(self, label="debug")
-        dbg.Bind(wx.EVT_BUTTON, self.OnDebug)
-
         hbox = wx.BoxSizer(wx.HORIZONTAL)
-        hbox.Add(dbg, flag=wx.ALIGN_BOTTOM)
-
         box.Add(hbox, proportion=0)
-
-        menu = wx.Menu()
-        dbg_it = wx.MenuItem(menu, wx.ID_ANY, "Debug")
-        self.Bind(wx.EVT_MENU, self.OnDebug, dbg_it)
-        accels = [wx.AcceleratorEntry(wx.ACCEL_CTRL, ord("D") , dbg_it.GetId())]
-        self.SetAcceleratorTable(wx.AcceleratorTable(accels))
-
-    def OnDebug(self, ev):
-        print "------DEBUG-----"
-        print "work size:  ", self.ctrl.Size
-        print "board size: ", self.ctrl.Board.Size
-        print "children: ", len(self.ctrl.Board.Selectables)
-        print "cards: ", self.ctrl.Deck.cards
 
 
 if __name__ == "__main__":
     app = wx.App()
     frame = TestFrame(None)
-
-    ####################
-    # write code here
-    ####################
-
-    ctrl = newgui.workspace.Workspace(frame)
-    frame.Sizer.Add(ctrl, proportion=1, flag=wx.EXPAND)
-    frame.ctrl = ctrl
-
-    # frame.ctrl.Deck.NewCard("Content", pos=(15,15))
-
-    w = newgui.board.ImagePlaceHolder(ctrl.Board)
-
-    # lis = utils.listener(topics=[utils.pub.ALL_TOPICS])
-
-    ####################
-    # end code here
-    ####################
-
-    frame.Show()
     app.MainLoop()
 
 
