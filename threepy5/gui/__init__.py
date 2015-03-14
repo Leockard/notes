10d25f8c8f75e0807403a66480b57fd3f5a42e60
leo <diegote142@gmail.com>
2015-03-14 09:33:12 -0500

    feat: with the coding of frame.py, gui/__init__ now runs the main frame.

--- threepy5/gui/__init__.py
+++ threepy5/gui/__init__.py
@@ -3,3 +3,12 @@ Modules in gui hold the view/controller classes for `threepy5`.
 """
 import board
 import workspace
+import frame
+
+
+
+if __name__ == "__main__":
+    import wx
+    app = wx.App()
+    frame = frame.ThreePyFiveFrame()
+    app.MainLoop()
